f0eecf0c4a3ec034a5db4c7efbd753ef32de3234
crobledo2 <crobledo@soaint.com>
2021-03-15 16:39:58 -0500

    first commit

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("prueba")
+public class Controller {
+
+
+
+
+    @GetMapping("/hola")
+    public String hola() {
+        return "hola";
+
+    }
+}
